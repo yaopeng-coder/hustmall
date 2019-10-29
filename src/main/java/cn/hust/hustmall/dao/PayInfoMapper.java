8829b8feff4315c6379c8c5cb62fe8278757f7a9
yaopeng-coder <m201973311@hust.edu.cn>
2019-10-29 14:16:50 +0800

    初始化项目，利用Mybatis 三剑客配置Pojo和dao

---
+++ src/main/java/cn/hust/hustmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package cn.hust.hustmall.dao;
+
+import cn.hust.hustmall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
